9f7a05d4146fbb68b36a1763cf2734e08155aa25
5n0weirdo <snoweirdo@protonmail.com>
2021-11-01 17:03:05 +0200

    Toggle functionailty added

--- src/Question.js
+++ src/Question.js
@@ -6,7 +6,9 @@ const Question = ({ title, info }) => {
     <article className="question">
       <header>
         <h4>{title}</h4>
-        <button className="btn">btn</button>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </header>
       {showInfo && <p>{info}</p>}
     </article>
