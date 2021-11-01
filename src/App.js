5c1f972df15b3e7fa2ec4e759a80b39a826ddef3
5n0weirdo <snoweirdo@protonmail.com>
2021-11-01 16:48:32 +0200

    Display question component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import questions from "./data";
+import data from "./data";
+import Question from "./Question";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  const { id, title, info } = data;
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answers about login</h3>
+        <section className="info"></section>
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </div>
+    </main>
+  );
 }
 
 export default App;
